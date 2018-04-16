89bd169239f8f90b82856b99e1f512a74c539984
Yoseph Solomon <3253217965@qq.com>
2018-04-16 09:01:57 +0300

    Added new customer add routing

---
+++ yosef_system/KS-yosef/src/app/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import * as moment from "moment";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginUserData = {}
+
+  constructor(private _auth: AuthService,
+    private _router: Router) { }
+
+  ngOnInit() {
+  }
+  loginUser(){
+    this._auth.loginUser(this.loginUserData)
+    .subscribe(
+      res => {
+        console.log(res)
+        const expiresAt = moment().add(res.expiresIn, 'second')
+
+        localStorage.setItem('id_token', res.idToken)
+        localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()))
+        // localStorage.setItem('token', res.token)
+        this._router.navigate(['/home'])
+      },
+      err => console.log(err)
+
+    )
+  }
+
+  // logout(){
+  //   localStorage.removeItem('id_token')
+  //   localStorage.removeItem('expires_at')
+  // }
+
+  // public isLoggedIn(){
+  //   return moment().isBefore(this.getExpiration())
+  // }
+  // isLoggedOut(){
+  //   return !this.isLoggedIn()
+  // }
+  // getExpiration(){
+  //   const expiration = localStorage.getItem('expires_at')
+  //   const expiresAt = JSON.parse(expiration)
+  //   return moment(expiresAt)
+  // }
+
+}
