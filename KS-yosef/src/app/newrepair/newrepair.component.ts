5f29b37aeb03e235d9ed41d5f9f31919bec29368
Yoseph Solomon <3253217965@qq.com>
2018-04-23 13:53:02 +0300

    Add repair view modified, data loaded from json format. User sign-out fixed.

--- KS-yosef/src/app/newrepair/newrepair.component.ts
+++ KS-yosef/src/app/newrepair/newrepair.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HistoryService } from '../history.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-newrepair',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewrepairComponent implements OnInit {
 
-  constructor() { }
+  garageHistory = []
+  constructor(private _historyService: HistoryService, private _router: Router) { }
 
   ngOnInit() {
+    this._historyService.getGarageHistory()
+      .subscribe(
+        res => this.garageHistory = res,
+        err => {
+          if (err instanceof HttpErrorResponse){
+            if (err.status === 401){
+              this._router.navigate(['/login'])
+
+            }
+          }
+        }
+      )
+      
   }
+ 
+  // deleteCustomer(Id){
+  //   var iAnswer = confirm("Are you sure you want to delete this Car ?");
+  //   if(iAnswer){
+  //     window.location = 'http://sakosys.com/temp/garage-management-system/repaircar/carlist.php?id=' + Id;
+  //     }
+  //   }
+              
+  //   $( document ).ready(function() {
+  //     setTimeout(function() {
+  //       $("#me").hide(300);
+  //       $("#you").hide(300);
+  //       }, 3000);
+  //     });
 
 }
