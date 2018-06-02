35194093420bf593a67980410c7f9e188d45cfa1
Yohannes Taye <yohannestaye83@gmail.com>
2018-06-02 12:15:27 +0300

    Show customer detail
    On the /customer page show the success modal when the view button is
    clicked.
    
    After executing the command npm install -D @types/jquery add the following
    code to the ts file of the customer-main.component.ts file [declare var
    jquery: any;] and [declare var $: any;]. Now you can write any jqury type
    of jquery code. To open the modal use the following snippet
    [$("modal-view").modal("show").] use a separate global variable to hold
    the currently selected modal to view, edit and delete the record.

--- yosef_system/KS-yosef/src/app/customer-main/customer-main.component.ts
+++ yosef_system/KS-yosef/src/app/customer-main/customer-main.component.ts
@@ -3,18 +3,19 @@ import { ClientService } from '../client.service';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 
+declare var  jquery: any; 
+declare var $:any;
+
 @Component({
   selector: 'app-customer-main',
   templateUrl: './customer-main.component.html',
   styleUrls: ['./customer-main.component.css']
 })
 export class CustomerMainComponent implements OnInit {
-
   clients = []
+  selectedClient: any = null;
   constructor(private _clientService: ClientService, private _router: Router) { }
-
   ngOnInit() {
-
     this._clientService.getClients()
       .subscribe(
         res => this.clients = res,
@@ -28,6 +29,21 @@ export class CustomerMainComponent implements OnInit {
         }
       )
       
+  }
+
+  btn_showClientInfoClick(client){
+    this.selectedClient = client;
+    $("#modal-view").modal("show");
+    console.log("show client button clicked");
+    console.log(client);
+  }
+  btn_editClientClick(){
+    $('#modal-edit').modal('show'); 
+    console.log("show edit client clicked");
+  }
+  btn_deleteClientClick(){
+    $('modal-delete').modal('show');
+    console.log("show delete client clicked"); 
   }
 
 }
