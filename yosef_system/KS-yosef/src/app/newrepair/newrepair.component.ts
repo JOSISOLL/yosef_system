a0a3f9148ca936321995150363f2a70ac97cd3cb
Yohannes Taye <yohannestaye83@gmail.com>
2018-06-04 12:55:49 +0300

    Create View Detail modal for repair list
    I created a new modal with a table and populated the rows with the fiels
    from the Repair model. To start the modal from the ts file I had to setup
    declare jquery as type. I used the selectedRepair global variable to track
    the currently selected row.

--- yosef_system/KS-yosef/src/app/newrepair/newrepair.component.ts
+++ yosef_system/KS-yosef/src/app/newrepair/newrepair.component.ts
@@ -3,16 +3,20 @@ import { HistoryService } from '../history.service';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Repair } from '../models/repair';
+declare var  jquery: any; 
+declare var $:any;
 
 @Component({
   selector: 'app-newrepair',
   templateUrl: './newrepair.component.html',
   styleUrls: ['./newrepair.component.css']
 })
-export class NewrepairComponent implements OnInit {
 
+export class NewrepairComponent implements OnInit {
   repairs = []
   closeResult: string;
+  selectedRepair: Repair;
   constructor(private _historyService: HistoryService, private _router: Router, private modalService: NgbModal) { }
 
   ngOnInit() {
@@ -35,7 +39,18 @@ export class NewrepairComponent implements OnInit {
       
       
   }
-
+  btn_showRepairInfoClick(repair: Repair){
+    console.log("show repair button clicked...");
+    this.selectedRepair =repair;
+    $("#modal-view").modal('show'); 
+    
+  }
+  btn_editRepairClick(repair: Repair){
+    console.log("edit repair clicked...");
+  }
+  btn_deleteRepairClick(repair: Repair){
+    console.log("delete repair clicked...");
+  }
   // open(content) {
   //   this.modalService.open(content).result.then((result) => {
   //     this.closeResult = `Closed with: ${result}`;
