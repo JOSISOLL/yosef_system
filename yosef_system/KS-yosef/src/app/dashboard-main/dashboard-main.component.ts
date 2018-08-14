0926655f7c78cfdc7e69fc1a2197e905d5dab847
Yoseph Solomon <3253217965@qq.com>
2018-08-14 18:14:40 +0300

    Main dashboard fixed. Customer. Repair, Stock and Supplier data fetched from database.

--- yosef_system/KS-yosef/src/app/dashboard-main/dashboard-main.component.ts
+++ yosef_system/KS-yosef/src/app/dashboard-main/dashboard-main.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HistoryService } from '../history.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { SuplierService } from '../services/suplier.service';
+import { PartsService } from '../services/parts.service';
+import { ClientService } from '../client.service';
 
 @Component({
   selector: 'app-dashboard-main',
@@ -7,9 +13,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardMainComponent implements OnInit {
 
-  constructor() { }
+  repairs = []
+  carsInRepair : number
+  partsInStock : number
+  clients : number
+  supliers : number
+  constructor(private _historyService: HistoryService, private _router : Router,private _partsService : PartsService, private _clientService : ClientService, private _suplerService : SuplierService) { }
 
   ngOnInit() {
+    this.getRepair();
+    this.getStock();
+    this.getCustomers();
+    this.getSupliers();
+    // console.log(this.repairs)
+  }
+  getSupliers(){
+    this._suplerService.getSupliers().subscribe(
+      res => this.supliers = res.length, 
+        err => {
+        if (err instanceof HttpErrorResponse){
+          if (err.status === 401){
+            this._router.navigate(['/login']);
+          }
+        }
+      }
+    )
+  }
+  getCustomers(){
+    this._clientService.getClients()
+      .subscribe(
+        res => {this.clients = res.length
+        console.log(this.clients)},
+        err => {
+          if (err instanceof HttpErrorResponse){
+            if (err.status === 401){
+              this._router.navigate(['/login'])
+
+            }
+          }
+      })
+  }
+  getStock(){
+    this._partsService.getStock()
+    .subscribe(
+      
+      res => {
+        this.partsInStock = res.length;
+      },
+
+      err => {
+        if (err instanceof HttpErrorResponse){
+          if (err.status === 401){
+            this._router.navigate(['/login']);
+          }
+        }
+      }
+    )
+  }
+  getRepair(){
+    this._historyService.getRepairs()
+      .subscribe(
+        res => {
+          this.repairs = res
+          // console.log(res)
+          this.carsInRepair = res.length
+          console.log(this.carsInRepair)
+          // console.log(this.repairs, this.repairs.length)
+        
+        },
+        err => {
+          if (err instanceof HttpErrorResponse){
+            if (err.status === 401){
+              this._router.navigate(['/login'])
+
+            }
+          }
+        }
+      )
   }
 
 }
